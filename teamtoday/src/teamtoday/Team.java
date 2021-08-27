7661c077a4ade84f3810b3ae20cce8473dd8fea1
mywesen <kebele41@yahoo.com>
2021-08-26 20:09:43 -0400

    Lets try

---
+++ teamtoday/src/teamtoday/Team.java
@@ -0,0 +1,13 @@
+package teamtoday;
+
+public class Team {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello There");
+
+	}
+
+}
